7199539bafcdb69d736067b98256ecf738b85432
Daniel Sienkiewicz <sienkiewicz@project-midas.com>
2015-10-18 15:32:12 +0200

    First version of PCF library

---
+++ PCF8574N/library/PCF8574N.cpp
@@ -0,0 +1,91 @@
+/****************************************************************************
+ * Copyright (C) 2015 by Daniel Sienkiewicz                                 *
+ *                                                                          *
+ *   PFC8574N is free software: you can redistribute it and/or modify it    *
+ *   under the terms of the GNU Lesser General Public License as published  *
+ *   by the Free Software Foundation, either version 3 of the License, or   *
+ *   (at your option) any later version.                                    *
+ *                                                                          *
+ *   PFC8574N is distributed in the hope that it will be useful,            *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *   GNU Lesser General Public License for more details.                    *
+ *                                                                          *
+ *   You should have received a copy of the GNU Lesser General Public       *
+ *   License along with Box. If not, see <http://www.gnu.org/licenses/>.    *
+ ****************************************************************************/
+
+/**
+ * @file PFC8574N.CPP
+ * @author Daniel Sienkiewicz
+ * @date 18 October 2015
+ */
+
+#include "PCF8574N.h"
+#import <Arduino.h>
+
+PCF8574N::PCF8574N(int address, int sda, int scl){
+	_address = address;
+	_sda = sda;
+	_scl = scl;
+
+	pinMode(sda, OUTPUT);
+	pinMode(scl, OUTPUT);
+	digitalWrite(scl, HIGH);
+	digitalWrite(sda, HIGH);
+}
+
+int PCF8574N::read8(){
+	int m, ack, answer = 0, d = 1;
+	digitalWrite(_sda, LOW);
+	delay(d);
+	digitalWrite(_scl, LOW);
+
+	for(m = 0x80; m; m >>= 1){ // address transfer MSB->LSB
+		if(_address & m)         
+			digitalWrite(_sda,HIGH);
+		else
+		digitalWrite(_sda,LOW);
+        
+	digitalWrite(_scl,HIGH); // generate clock pulse
+	delay(d);
+	digitalWrite(_scl,LOW); 
+	delay(d);
+}
+
+   pinMode(_sda,INPUT);
+   digitalWrite(_scl,HIGH);
+   delay(d);
+   
+   //Read ACL
+   ack = digitalRead(_sda);
+   digitalWrite(_scl,LOW);
+   delay(d);
+   
+   for(m = 0x80; m; m>>=1){
+		digitalWrite(_scl,HIGH);
+		delay(d);
+    
+		if(digitalRead(_sda)){
+			answer |= m;
+		}
+     
+		digitalWrite(_scl, LOW);
+		delay(d);
+	}
+   
+	// Generate STOP bit
+	pinMode(_sda,OUTPUT);
+	digitalWrite(_scl, HIGH);
+	delay(d);
+	digitalWrite(_sda, HIGH);
+	delay(d);
+	return answer;
+}
+
+//TO DO
+int PCF8574N::write8(int data){
+	int answer;
+	
+	return answer;
+}
